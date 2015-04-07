4c894e2049bad376d4d2b127bfa4a3fc974fd644
ahmadhidayat <medhidayat9@gmail.com>
2015-04-07 21:11:53 +0700

    commit pertama

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TugasTiga::Application.config.secret_key_base = '5c795fe9e3e33ce08bd42cbd7cdd6b1241d0d69bc13a26a249f244aaed30e10f12231bbf1aefeb927846fca6138c71471d28bf3fc4b9bc30bd9c118e281a4688'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TugasTiga::Application.config.session_store :cookie_store, key: '_Tugas_tiga_session'
